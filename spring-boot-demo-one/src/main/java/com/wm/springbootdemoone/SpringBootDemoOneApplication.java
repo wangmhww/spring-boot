a8e8d932f31518fd858510a1a53f46467ad698d0
wangmingf <wangmingf@dcits.com>
2020-10-29 10:03:14 +0800

    首次提交

---
+++ spring-boot-demo-one/src/main/java/com/wm/springbootdemoone/SpringBootDemoOneApplication.java
@@ -0,0 +1,13 @@
+package com.wm.springbootdemoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoOneApplication.class, args);
+    }
+
+}
---
+++ spring-boot-demo-one/src/test/java/com/wm/springbootdemoone/SpringBootDemoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wm.springbootdemoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
