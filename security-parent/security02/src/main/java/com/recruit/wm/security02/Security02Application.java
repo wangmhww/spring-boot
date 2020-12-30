fb29da68dbc357a98f2cb8b46185136347587d10
wangmingf <wangmingf@dcits.com>
2020-12-30 18:00:21 +0800

    security02提交

---
+++ security-parent/security02/src/main/java/com/recruit/wm/security02/Security02Application.java
@@ -0,0 +1,13 @@
+package com.recruit.wm.security02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.recruit.wm.security02"})
+public class Security02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security02Application.class, args);
+    }
+
+}
