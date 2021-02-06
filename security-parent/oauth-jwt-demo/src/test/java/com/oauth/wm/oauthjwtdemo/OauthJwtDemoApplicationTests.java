4140823b24d1f8d9c915f6d5a653593367bdd1dc
wangmhww <wangmhww@163.com>
2021-02-06 16:11:02 +0800

    oauth-jwt提交-修改有效期时间

---
+++ security-parent/oauth-jwt-demo/src/test/java/com/oauth/wm/oauthjwtdemo/OauthJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.wm.oauthjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthJwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
